234e0e5257250abeeca842b90cd066d161ddd1ce
abdullah <abdullahMustafa2000>
2023-03-17 07:27:43 +0200

    elements design

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
-import 'package:etouch/ui/screens/splashscreen.dart';
+import 'package:etouch/ui/fake_landing.dart';
+import 'package:etouch/ui/themes/theme_manager.dart';
+import 'package:etouch/ui/themes/themes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+ThemeManager _themeManager = ThemeManager();
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'ETouch',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: _themeManager.themeMode,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: SplashScreen(),
+      home: FakeLanding(),
     );
   }
 }
+
+
+//on switch clicked: themeManager.toggleTheme(on|off)
+//switch state: _themeManager.themeMode == ThemeMode.dark
